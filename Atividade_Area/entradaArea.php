3744dece80eb46430d3f1c48e0b32360319e31b3
arthur-oliveiraa <arthur.b.oliveira@edu.senai.br>
2026-04-28 10:43:54 -0300

    v.0.2 - Versão com a conversão de moedas!

--- Atividade_Area/entradaArea.php
+++ Atividade_Area/entradaArea.php
@@ -9,6 +9,13 @@ use function geometria\areaTriangulo;
 use function geometria\areaCirculo;
 use function geometria\areaTrapezio;
 
+use function Conversão\dolarParaReal;
+use function Conversão\euroParaReal;
+use function Conversão\pesoParaReal;
+use function Conversão\libraParaReal;
+use function Conversão\ieneParaReal;
+
+
 echo "CÁLCULO DE ÁREAS \n";
 echo "--------------------------\n";
 $resultado = 0;
@@ -32,3 +39,23 @@ $baseMenor = 5;
 $altura = 5;
 $resultado = areaTrapezio($baseMaior, $baseMenor, $altura, $resultado);
 echo "Área do Trapézio: " . $resultado . "\n";
+
+echo "\nCONVERSÃO DE MOEDAS \n";
+echo "--------------------------\n";
+$dolar = 5.00;
+$real = 1.00;
+$conv = 0;
+$conv = dolarParaReal($dolar, $real, $conv);
+echo "Dolar para Real: " . $conv . "\n";
+$euro = 5.50;
+$conv = euroParaReal($euro, $real, $conv);
+echo "Euro para Real: " . $conv . "\n";
+$peso = 0.25;
+$conv = pesoParaReal($peso, $real, $conv);
+echo "Peso para Real: " . $conv . "\n";
+$libra = 6.74;
+$conv = libraParaReal($libra, $real, $conv);
+echo "Libra para Real: " . $conv . "\n";
+$iene = 0.31;
+$conv = ieneParaReal($iene, $real, $conv);
+echo "Iene para Real: " . $conv . "\n";
