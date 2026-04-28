f8949d075cb2291fc00c034eb5769bdefff04ea8
arthur-oliveiraa <arthur.b.oliveira@edu.senai.br>
2026-04-28 09:59:39 -0300

    v.0.1 - Biblioteca Area

---
+++ entradaDados2.php
@@ -0,0 +1,34 @@
+<?php
+
+
+require_once "bibliotecaFuncoes2.php";
+
+use function geometria\areaQuadrado;
+use function geometria\areaRetangulo;
+use function geometria\areaTriangulo;
+use function geometria\areaCirculo;
+use function geometria\areaTrapezio;
+
+echo "CÁLCULO DE ÁREAS \n";
+echo "--------------------------\n";
+$resultado = 0;
+$lado = 5;
+$resultado = areaQuadrado($resultado, $lado);
+echo "Área do Quadrado: " . $resultado . "\n";
+$base = 10;
+$altura = 5;
+$resultado = areaRetangulo($resultado, $base, $altura);
+echo "Área do Retângulo: " . $resultado . "\n";
+$base = 10;
+$altura = 5;
+$resultado = areaTriangulo($resultado, $base, $altura);
+echo "Área do Triângulo: " . $resultado . "\n";
+$raio = 5;
+$resultado = 0;
+$resultado = areaCirculo($resultado, $raio);
+echo "Área do Círculo: " . $resultado . "\n";
+$baseMaior = 10;
+$baseMenor = 5;
+$altura = 5;
+$resultado = areaTrapezio($baseMaior, $baseMenor, $altura, $resultado);
+echo "Área do Trapézio: " . $resultado . "\n";
